6066e22a312e174dea2d97ae79ebdc9015db6058
MoathDawahreh <moath.dawahreh@yahoo.com>
2016-12-13 17:22:56 +0200

    the..

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+  	return val;
   };
 
   /**
@@ -37,7 +38,17 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-  };
+  	// if (n=== undefined){
+  	// 	return array[array.length-1];
+  	// }else if(n===0) {
+  	// 	return []; 
+  	// } else if(n>array.length){
+  	// 		return array.slice(0,n);
+  	// } else {
+  	// 			return array.slice(array.length-n , array.length);
+  	// 			}
+	return n=== undefined ? array[array.length-1] : n===0 ? [] : n>array.length ? array.slice(0,n): array.slice(array.length-n , array.length);
+  	};
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
@@ -45,6 +56,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+	if (Array.isArray(collection)){
+		for (var i = 0; i < collection.length; i++) {
+			iterator(collection[i],i,collection)
+		}
+	}else {
+		for (var key in collection){
+			iterator(collection[key],key,collection )
+		}
+	}
+	
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,21 +87,49 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+  	var acc=[];
+  	_.each(collection,function(element,i){
+  		if (test(element,i)) {
+  			acc.push(element)
+  		}
+  	})
+  	return acc;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
+  	var acc=[];
+  	_.each(collection,function(element,i){
+  		if (!test(element,i)) {
+  			acc.push(element)
+  		}
+  	})
+  	return acc;
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+  	var arr2=[];
+  	_.each(array,function(element,i){
+  		if (_.indexOf(arr2,element)==-1) 
+  			arr2.push(element);
+  	})
+  	return arr2;
   };
 
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+  	var acc=[];
+  	if (!Array.isArray(collection)) {
+  		acc={};
+  	}
+  	_.each(collection,function(element,key){
+  		acc[key]=iterator(element,key);
+  	})
+  	return acc;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
@@ -125,6 +174,22 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+  	var arr=[];
+  	if (!Array.isArray(collection)) {
+  		arr=Object.keys(collection);
+  		accumulator=collection[arr[0]];
+  	}
+
+  	else if (accumulator===undefined) {
+  		accumulator=collection[0];
+  		collection=collection.slice(1);
+  	}
+
+  	_.each(collection,function(element,key){
+  		accumulator=iterator(accumulator,element , key)
+  	})
+  	return accumulator;
+
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -136,6 +201,7 @@
         return true;
       }
       return item === target;
+
     }, false);
   };
 
@@ -143,6 +209,9 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return reduce(collection,function(result,elements){
+    	return result && iterator(elements)
+    },true)
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
